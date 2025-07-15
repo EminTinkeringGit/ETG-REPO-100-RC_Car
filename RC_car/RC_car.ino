5e2c5dbd9ab303335127891910fcbe123dff9f53
Emin <sayedamin@hotmail.it>
2025-07-16 00:39:49 +0200

    Update RC_car.ino
    added servo functionality

--- RC_car/RC_car.ino
+++ RC_car/RC_car.ino
@@ -1,11 +1,24 @@
+#include <Servo.h>
 
 // THROTTLE PARAMETERS
-int min_pulse_th = 1000;
+int min_pulse_th = 1070;
 int mid_pulse_th = 1600;
-int max_pulse_th = 2000;
+int max_pulse_th = 2137;
 int dead_zone_th = 20;
 int throttle;
 unsigned long pulse_throttle;
+
+// RECEVING CHANNELS - THROTTLE
+const int RX_throttle = 5;
+const int throttle_pwm_pin = 10;
+const int dir_A_throttle = 12;
+const int dir_B_throttle = 13;
+
+// STEERING SERVO
+Servo servo_motor;
+int steering_angle;
+const int servo_pwm = 6;  
+const int RX_steering = 3;
 
 // STEERING PARAMETERS
 int min_pulse_st = 1000;
@@ -14,16 +27,10 @@ int max_pulse_st = 2000;
 int dead_zone_st = 20;
 int steering;
 unsigned long pulse_steering;
+  
 
-// RECEVING CHANNELS
-const int RX_throttle = 5;
-const int RX_steering = 6;
-const int throttle_pwm_pin = 10;
-const int steering_pwm_pin = 11;
-const int dir_A_throttle = 12;
-const int dir_B_throttle = 13;
-const int dir_A_steering = 3;
-const int dir_B_steering = 4;
+
+// SETUP FUNCTION
 void setup() {
 
   Serial.begin(9600);
@@ -33,23 +40,22 @@ void setup() {
   pinMode(throttle_pwm_pin, OUTPUT);
   pinMode(dir_A_throttle, OUTPUT);
   pinMode(dir_B_throttle, OUTPUT);
+
+  // STEERING SERVO PIN SETUP
+  servo_motor.attach(servo_pwm);
   
   // STEERING PINs SETUP
   pinMode(RX_steering, INPUT);
-  pinMode(steering_pwm_pin, OUTPUT);
-  pinMode(dir_A_steering, OUTPUT);
-  pinMode(dir_B_steering, OUTPUT);
 
-  // IC
+  // IC (Initial Conditions)
   digitalWrite(dir_A_throttle, LOW);
   digitalWrite(dir_B_throttle, HIGH);
-  digitalWrite(dir_A_steering, LOW);
-  digitalWrite(dir_B_steering, HIGH);
 }
 
+// LOOP FUNCTION
 void loop() {
 
-  // THROTTLE
+  // THROTTLE CONTROL
   pulse_throttle = pulseIn(RX_throttle, HIGH, 25000); // Timeout after 25ms
 
   if (pulse_throttle < (mid_pulse_th - dead_zone_th)) {
@@ -68,9 +74,18 @@ void loop() {
     digitalWrite(dir_B_throttle, LOW);
   }
 
-  // STEERING
+  // STEERING CONTROL
   pulse_steering = pulseIn(RX_steering, HIGH, 25000); // Timeout after 25ms
 
+  if (pulse_steering < (mid_pulse_st - dead_zone_st)) {
+    steering_angle = map(pulse_steering, min_pulse_st, mid_pulse_st, 0, 89);
+    servo_motor.write(steering_angle);
+  } else if (pulse_steering > (mid_pulse_st + dead_zone_st)) {
+    steering_angle = map(pulse_steering, mid_pulse_st, max_pulse_st, 91, 180);
+    servo_motor.write(steering_angle);
+  } else {
+    servo_motor.write(90); // Neutral - 90 degrees
+  }
 
-  delay(10); // Read every 100ms
+  Serial.println("RX throttle pulse: " + String(pulse_throttle) + "// RX steering pulse: " + String(pulse_steering));
 }
